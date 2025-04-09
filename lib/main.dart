c8c1f2bd023bf9ed7c2fd159181d18270a159c3e
KISHORE.S <kishoreshanmugam2003@gmail.com>
2025-04-09 15:26:27 +0530

    firstcommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:bhargavi_oil_masala/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:bhargavi_oil_masala/app.dart';
 
-void main() {
+Future<void>  main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
-}
+} 
---
+++ lib/screens/getstart_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GetstartPage extends StatefulWidget {
+  const GetstartPage({super.key});
+
+  @override
+  State<GetstartPage> createState() => _GetstartPageState();
+}
+
+class _GetstartPageState extends State<GetstartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
